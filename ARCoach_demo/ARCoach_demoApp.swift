6a31f52d1cc2ae741f8617f94eb58d6b449ff0ee
dvalab <wenhui9703@gmail.com>
2025-10-28 20:07:55 +0800

    fix: update immersion style to mixed

--- ARCoach_demo/ARCoach_demoApp.swift
+++ ARCoach_demo/ARCoach_demoApp.swift
@@ -35,6 +35,6 @@ struct ARCoach_demoApp: App {
                     avPlayerViewModel.reset()
                 }
         }
-        .immersionStyle(selection: .constant(.full), in: .full)
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
     }
 }
